78ef97a385b5b23f156126510d730c5e7dfa03de
Ifriism <idafm2@hotmail.com>
2025-05-22 22:09:19 +0200

    Added footer + links

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-    </div>
-   
+   <footer>Coded by Ida Friis-Mikkelsen, hosted on <a href="https://github.com/Ifriism/react-weather-app" rel="noopener">GitHub</a> and open-sourced on <a href="https://weatherfrenzy.netlify.app/" rel="noopener">Netlify</a></footer>
+  </div>
   );
 }
 
