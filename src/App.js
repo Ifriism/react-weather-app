5a8efa1732d21faca86973d91513d30000c8b000
Ifriism <idafm2@hotmail.com>
2025-05-25 19:02:13 +0200

    Added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+    <div className="container">
+      <Weather />
    <footer>Coded by Ida Friis-Mikkelsen, hosted on <a href="https://github.com/Ifriism/react-weather-app" rel="noopener">GitHub</a> and open-sourced on <a href="https://weatherfrenzy.netlify.app/" rel="noopener">Netlify</a></footer>
+  </div>
   </div>
   );
 }
